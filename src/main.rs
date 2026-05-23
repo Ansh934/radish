e998c4995cd6d04dd41abe7f7b294d79d201a91a
Ansh934 <soni.ansh934@gmail.com>
2026-05-23 16:47:30 +0530

    implement server module with asynchronous TCP connection handling

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,11 @@
 mod resp;
+mod cmd;
+mod server;
 
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use server::Server;
 
 #[tokio::main]
 async fn main() {
     println!("Logs from your program will appear here!");
-
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-
-    loop {
-        let stream = listener.accept().await;
-        match stream {
-            Ok((mut _stream, addr)) => {
-                println!("accepted new connection from {}", addr);
-
-                tokio::spawn(async move {
-                    let mut buf = [0; 1024];
-                    loop {
-                        // will it get interupted by system signal?
-                        let read_count = _stream.read(&mut buf).await.unwrap();
-                        if read_count == 0 {
-                            break;
-                        }
-                        _stream.write_all(&buf).await.unwrap();
-                    }
-                });
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
-    }
+    Server::start().await;
 }
---
+++ src/server.rs
@@ -0,0 +1,50 @@
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
+use crate::cmd::RadishCommand;
+
+pub(crate) struct Server {}
+
+impl Server {
+    pub(crate) async fn start() -> Result<(), Box<dyn std::error::Error>> {
+        let listener = TcpListener::bind("127.0.0.1:6379").await?;
+        loop {
+            let (mut stream, addr) = listener.accept().await?;
+            println!("accepted new connection from {}", addr);
+            tokio::spawn(async move {
+                let mut buf = [0; 512];
+
+                loop {
+                    match stream.read(&mut buf).await {
+                        Ok(0) => {
+                            println!("client disconnected");
+                            break;
+                        }
+
+                        Ok(read_count) => match RadishCommand::from_bytes(&buf[..read_count]) {
+                            Some(cmd) => {
+                                let response = cmd.eval();
+                                if let Err(err) = stream.write_all(&response).await {
+                                    eprintln!("write error: {}", err);
+                                    break;
+                                }
+                            }
+                            None => {
+                                let error_response = b"-ERR invalid command\r\n";
+                                if let Err(err) = stream.write_all(error_response).await {
+                                    eprintln!("write error: {}", err);
+                                    break;
+                                }
+                            }
+                        },
+
+                        Err(err) => {
+                            eprintln!("read error: {}", err);
+                            break;
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
