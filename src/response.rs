b4b9e50f8e5992ba6d9462656e389118924852b4
Ansh934 <soni.ansh934@gmail.com>
2026-05-24 18:48:04 +0530

    feat: add Response struct and eval function for command evaluation

---
+++ src/response.rs
@@ -0,0 +1,36 @@
+use crate::cmd::{CommandType, RadishCommand};
+use crate::resp::{Resp, RespValue};
+use crate::store::SharedStore;
+
+pub(crate) struct Response {
+    pub(crate) data: Vec<u8>,
+}
+
+impl Response {
+    pub(crate) fn eval(cmd: &RadishCommand, store: &SharedStore) -> Self {
+        let data = match cmd.cmd_type() {
+            CommandType::Ping => Resp::encode_simple_string("PONG"),
+            CommandType::Echo => {
+                if let Some(arg) = cmd.args().get(0) {
+                    Resp::encode_string(arg)
+                } else {
+                    Resp::encode_error("ECHO command requires an argument")
+                }
+            }
+            CommandType::Set => {
+                if let (Some(key), Some(value)) = (cmd.args().get(0), cmd.args().get(1)) {
+                    // Borrow the RefCell mutably and set the value without Mutex locking
+                    let mut store_ref = store.borrow_mut();
+                    store_ref.set(key.clone(), RespValue::SimpleString(value.clone()), None);
+                    Resp::encode_simple_string("OK")
+                } else {
+                    Resp::encode_error("SET command requires a key and a value")
+                }
+            }
+            CommandType::Get => Resp::encode_error("GET not implemented yet"),
+            CommandType::Ttl => Resp::encode_error("TTL not implemented yet"),
+            CommandType::Unknown(name) => Resp::encode_error(&format!("unknown command: {}", name)),
+        };
+        Response { data }
+    }
+}
